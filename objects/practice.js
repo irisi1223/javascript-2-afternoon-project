74fe2b6e397585b46f9ef6fc6be96fa88dd9f159
irisi1223 <jbushman442@gmail.com>
2020-06-04 16:45:27 -0600

    answerd question edited

--- objects/practice.js
+++ objects/practice.js
@@ -14,8 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: "Jacob",
+  age: 25
 
+}
+alert(me.name);
 ////////// PROBLEM 2 //////////
 
 /*
@@ -23,21 +27,30 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: "ThreeDaysGrace",
+  food: 'bacon',
+  person: 'dad',
+  book: 'TheTransalSaga',
+  movie: 'avatar',
+  holiday: 'Halloween'
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'shellby'
+favoriteThings.brand = 'underArmour'
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'donuts'
+favoriteThings.book = 'ofMiceAndMen'
 
 ////////// PROBLEM 3 //////////
 
@@ -48,14 +61,18 @@
   Set the value of that key to 'chapstick'.
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
+let backpack = {
 
-//Code here
+}
+let item = 'firstPocket';
+backPack [item] = 'chapstick'
+backPack.color = 'blue'
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -64,7 +81,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
-
+console.log(backPack);
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -85,7 +102,8 @@ var user2 = {
 */
 
 //Code Here
-
+user2.name = 'Bryan G. Smith';
+user2.email = 'bryan.smith@devmouta.in'
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
@@ -96,19 +114,28 @@ var user2 = {
 */
 
 //Code Here
+let methodCollection = {
 
+}
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
 
 //Code Here
-
+methodCollection.alertHello = function (){
+  alert("hello");
+}
+methodCollection.logHello = function (){
+  console.log("hello");
+}
 /*
   Now call your alertHello and logHello methods.
 */
 
 //Code Here
+methodCollection.alertHello();
+methodCollection.logHello();
 
 ////////// PROBLEM 6 //////////
 
@@ -118,7 +145,14 @@ var user2 = {
 */
 
 //Code Here
-
+function makePerson(name, bday, ssn){
+  let person = {
+    name: name,
+    birthday: bday,
+    ssn: ssn
+  }
+  return person;
+}
 ////////// PROBLEM 7 //////////
 
 /*
@@ -127,3 +161,11 @@ var user2 = {
 */
 
 //Code Here
+function makeCard(cardNumber, expirationDate, securityCode){
+  let card = {
+    cardNumber: 1111,
+    expirationDate: 12/22,
+    securityCode: 2222
+  }
+  return card;
+}
